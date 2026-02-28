246139e79ade5f68714a813105bd35859587b17b
HoelK <KeromnesHoel@gmail.com>
2026-02-28 23:56:04 +0100

    added getters for db querries

---
+++ backend/auth/src/db/schemas/getters.ts
@@ -0,0 +1,7 @@
+import { db }       from "../init"
+import { users }    from "./users"
+import { eq }       from "drizzle-orm"
+
+export function getUserByID(id: number)				{ return (db.select().from(users).where(eq(users.id, id)).limit(1)); }
+export function getUserByEMAIL(email: string)		{ return (db.select().from(users).where(eq(users.email, email)).limit(1)); }
+export function getUserByUSERNAME(username: string)	{ return (db.select().from(users).where(eq(users.username, username)).limit(1)); }
