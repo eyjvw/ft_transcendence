baa21c2ebc9e44aae704af0d6ea1d31ca5f213e4
HoelK <KeromnesHoel@gmail.com>
2026-03-01 00:31:38 +0100

    improved readability

--- backend/auth/src/controllers/me.ts
+++ backend/auth/src/controllers/me.ts
@@ -1,8 +1,7 @@
-import { db }			from "../db/init";
-import { users }		from "../db/schemas/users";
-import { eq }			from "drizzle-orm";
 import { verifyJWT }	from "../jwt/jwt";
 import { StatusCode }	from "../types/status_code.ts";
+import * as errors		from "../utils/errors.ts"
+import * as queries		from "../db/schemas/getters.ts"
 
 export async function meController(req: Request): Promise<Response>
 {
@@ -11,35 +10,35 @@ export async function meController(req: Request): Promise<Response>
 		const cookie: string | null = req.headers.get("cookie");
 
 		if (!cookie)
-			return new Response(JSON.stringify({ error: "No Cookie" }), { status: StatusCode.BAD_REQUEST });
+			return (errors.noCookie());
 
 		const token: string | undefined = cookie.match(/token=([^;]+)/)?.[1];
 
 		if (!token)
-			return new Response(JSON.stringify({ error: "Unauthorized" }), { status: StatusCode.UNAUTHORIZED });
+			return (errors.unauthorized());
 
 		const { uid }: Record<string, string> = await verifyJWT(token);
-		const user = await db.select().from(users).where(eq(users.id, Number(uid))).limit(1);
+		const [user] = await queries.getUserByID(Number(uid));
 
-		if (user.length < 1)
-			return new Response(JSON.stringify("User not found"), { status: StatusCode.NOT_FOUND });
+		if (!user)
+			return (errors.userNotFound());
 
 		return new Response(JSON.stringify({
 			user: {
-				id: user[0]!.id,
-				username: user[0]!.username,
-				email: user[0]!.email,
-				avatarUrl: user[0]!.avatar_url,
-				isActive: Boolean(user[0]!.is_active),
-				language: user[0]!.language,
-				coins: user[0]!.coins,
-				authProvider: user[0]!.auth_provider
+				id:				user.id,
+				username:		user.username,
+				email:			user.email,
+				avatarUrl:		user.avatar_url,
+				isActive:		Boolean(user.is_active),
+				language:		user.language,
+				coins:			user.coins,
+				authProvider:	user.auth_provider
 			}
 		}), { status: StatusCode.OK });
 	}
 	catch(err: unknown)
 	{
 		console.error(err);
-		return new Response(JSON.stringify({ error: "Server Error" }), { status: StatusCode.INTERNAL_SERVER_ERROR });
+		return (errors.server());
 	}
 }
